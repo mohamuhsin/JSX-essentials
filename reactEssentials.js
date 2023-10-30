c33123bdbd0f2b1b1e968e344c572525ee66ca32
mohamuhsin <muhsinohoho@outlook.com>
2023-10-30 18:23:56 +0300

    JSX concepts

--- reactEssentials.js
+++ reactEssentials.js
@@ -148,3 +148,19 @@ const toDoList = (
 //const container = document.getElementById("app");
 //const root = createRoot(container);
 root.render(toDoList);
+
+//9. VIRTUAL DOM:
+
+/*One special thing about a React root’s render() method is that
+ it only updates DOM elements that have changed.
+
+That means that if you render the exact same thing twice in a row,
+ the second render will do nothing:*/
+
+const hello = <h1>Hello world</h1>;
+
+// This will add "Hello world" to the screen:
+root.render(hello, document.getElementById("app"));
+
+// This won't do anything at all:
+root.render(hello, document.getElementById("app"));
